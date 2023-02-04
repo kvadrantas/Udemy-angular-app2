91370807492acfa5e9d9118c4751838b0791abe2
Rolandas Seputis <vi.kvadrantas@gmail.com>
2023-02-04 13:14:55 +0200

    10. Passing properties to Custom Directive (Binding to Directive)

--- src/app/directives/greenbg.directive.ts
+++ src/app/directives/greenbg.directive.ts
@@ -1,25 +1,28 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[greenBg]'
 })
 export class GreenbgDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent'
+  @Input() hoverColor: string = 'yellow'
+  @HostBinding('style.backgroundColor') bgColor!: string;
 
   constructor(private elRef: ElementRef) { 
   }
 
   ngOnInit(): void {
+    this.bgColor = this.defaultColor
   } 
 
-  @HostBinding('style.backgroundColor') bgColor: string = 'transparent'
 
   @HostListener('mouseenter') onMouseover(evt: Event) {
     // this.elRef.nativeElement.style.background = 'yellow'
-    this.bgColor = 'yellow'
+    this.bgColor = this.hoverColor
   }
   
   @HostListener('mouseleave') onMouseleave(evt: Event) {
     // this.elRef.nativeElement.style.background = 'transparent'
-    this.bgColor = 'transparent'
+    this.bgColor = this.defaultColor
   }
 }
