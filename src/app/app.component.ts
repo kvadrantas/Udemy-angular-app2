92ba2441c4ea92ee7c08742004cefc0e824f9536
Rolandas Seputis <vi.kvadrantas@gmail.com>
2023-02-19 14:33:19 +0200

    16. Routes - Navigation Programatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-app1';
-  value1FromParent = "Tekstas111"
+  value1FromParent = 'Tekstas111';
 
-    
   onButtonClick() {
     console.log('clicked');
     this.value1FromParent += 'a';
+  }
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  navigateOnClick() {
+    this.router.navigate(['/page2']);
+    // If you want to use relative path in programatic route
+    // this.router.navigate(['/page2'], { relativeTo: this.activatedRoute });
   }
 }
